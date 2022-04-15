d308eb4a8860ca3a0b2cb517cde04ffbaf733d36
Vishwas <baghel.vishwas0@gmail.com>
2022-04-15 09:24:02 +0530

    [Vishwas]add: Fibonacci Series Problem Added

---
+++ LogicalProblems/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            int num, a = 0, b = 1, c;
+            Console.WriteLine("Enter any number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= num; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+        }
+ 
+    }
+}
--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace LogicalProblems
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Logical Problems");
+            bool end = true;
+            Console.WriteLine("1. Fibonacci Seriesn\n2. End of the program");
+            while (end == true)
+            {
+                Console.WriteLine("Choose the program to execute: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries fiboSeries = new FibonacciSeries();
+                        fiboSeries.Fibonacci();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter the valid option :");
+                        break;
+
+                        
+                }
+            }
+        }
+    }
+}
